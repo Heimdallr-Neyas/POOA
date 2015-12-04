50f1eb2e549ebced5d11db0c7a04ec12164e5a64
Benoit <Brimeux.Benoit@gmail.com>
2015-12-04 11:58:52 +0100

    Modification de Paint pour une interface graphique plus claire.

--- src/drawing/Paint.java
+++ src/drawing/Paint.java
@@ -1,14 +1,14 @@
 package drawing;
 
 import java.awt.BorderLayout;
-
 import java.awt.Color;
 
+import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.Box;
 
 /**
  * Classe Interface graphique pour l'application de dessin
@@ -16,6 +16,7 @@ import javax.swing.Box;
 public class Paint {
 
 	private JFrame frame;
+	
 	private JButton clearButton;
 	private JButton circleButton;
 	private JButton rectangleButton;
@@ -25,32 +26,29 @@ public class Paint {
 	private JButton UndoButton;
 	private JButton RedoButton;
 	private JButton addTextButton;
-	private JPanel buttonPanel;
+	private JLabel counter;
+	
 	private JPanel mainPanel;
-	private JPanel infoPanel;
+	private Box buttonPanel;
+	private Box infoPanel;
 	private Box PanelBas;
 	private Box PanelGroup;
-	private Box PanelDuplicate;
-	private Box PanelRedo;
-	private Box PanelText;
+	
 	private Drawing drawing;
 	private JTextField texte;
-	private JTextField texteForme;
 	
 	public void run(){
 		frame = new JFrame("Paint");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainPanel = new JPanel(new BorderLayout());
-		PanelBas = Box.createVerticalBox();
-		PanelGroup = Box.createHorizontalBox();
-		PanelDuplicate = Box.createHorizontalBox();
-		PanelRedo = Box.createHorizontalBox();
-		PanelText = Box.createHorizontalBox();
+		
+		
+
 		
 		drawing = new Drawing();
 		drawing.setBackground(Color.WHITE);
 		
 		
+		// Les boutons
 		clearButton = new JButton("Clear");
 		circleButton = new JButton("Circle");
 		rectangleButton = new JButton("Rectangle");
@@ -61,42 +59,7 @@ public class Paint {
 		RedoButton = new JButton("Redo");
 		addTextButton = new JButton("add Text");
 		
-		buttonPanel = new JPanel();
-		buttonPanel.add(clearButton);
-		buttonPanel.add(circleButton);
-		buttonPanel.add(rectangleButton);
-		
-		PanelGroup.add(groupButton);
-		PanelGroup.add(degroupButton);
-		
-		PanelDuplicate.add(duplicateButton);
-		
-		PanelRedo.add(UndoButton);
-		PanelRedo.add(RedoButton);
-		
-		texteForme = new JTextField("Saisir le texte");
-
-		PanelText.add(addTextButton);
-		PanelText.add(texteForme);
-		
-		
-		texte = new JTextField(drawing.getNumber());
-		
-		mainPanel.add(PanelBas, BorderLayout.SOUTH);
-		mainPanel.add(drawing, BorderLayout.CENTER);
-		
-		infoPanel = new JPanel();
-		infoPanel.add(texte);
-		
-		PanelBas.add(buttonPanel);
-		PanelBas.add(infoPanel);
-		PanelBas.add(PanelGroup);
-		PanelBas.add(PanelDuplicate);
-		PanelBas.add(PanelRedo);
-		PanelBas.add(PanelText);
-		
-		texte.addActionListener(new CounterTextListener(texte, drawing));
-		
+		counter = new JLabel("Nombre de figures : ");
 		
 		//listeners pour les boutons
 		//clearButton.addActionListener(new ClearButtonListener(drawing));
@@ -111,13 +74,45 @@ public class Paint {
 		UndoButton.addActionListener(inv);
 		RedoButton.addActionListener(inv);
 		addTextButton.addActionListener(inv);
+	
+
 		
+		// Gestion des Panels 
+		mainPanel = new JPanel(new BorderLayout());
+		PanelBas = Box.createVerticalBox();
+		PanelGroup = Box.createHorizontalBox();
+		buttonPanel = Box.createHorizontalBox();
+		infoPanel = Box.createHorizontalBox();
+		
+		mainPanel.add(PanelBas, BorderLayout.SOUTH);
+		mainPanel.add(drawing, BorderLayout.CENTER);
+		
+		
+		buttonPanel.add(clearButton);
+		buttonPanel.add(circleButton);
+		buttonPanel.add(rectangleButton);
+		
+		texte = new JTextField(drawing.getNumber());
+		texte.addActionListener(new CounterTextListener(texte, drawing));
+		infoPanel.add(counter);
+		infoPanel.add(texte);
+		
+		PanelGroup.add(groupButton);
+		PanelGroup.add(degroupButton);
+		PanelGroup.add(duplicateButton);
+		PanelGroup.add(UndoButton);
+		PanelGroup.add(RedoButton);
+		PanelGroup.add(addTextButton);
+		
+		PanelBas.add(buttonPanel);
+		PanelBas.add(infoPanel);
+		PanelBas.add(PanelGroup);
+		
+
 		//listeners pour la zone de dessin
 		DrawingMouseListener l = new DrawingMouseListener(drawing, texte);
 		drawing.addMouseListener(l);
-		drawing.addMouseMotionListener(l);
-		
-		
+		drawing.addMouseMotionListener(l);		
 
 		frame.getContentPane().add(mainPanel);
 		frame.setSize(640,580);
